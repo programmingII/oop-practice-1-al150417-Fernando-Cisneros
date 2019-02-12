6267ce9081ee568897be5f8b347ce140724dac15
al150417-Fernando-Cisneros <47366643+al150417-Fernando-Cisneros@users.noreply.github.com>
2019-02-12 13:34:27 -0700

    Add files via upload

---
+++ programa8JAVA.java
@@ -0,0 +1,18 @@
+// Fernando Joel Cisneros 
+// Time   13:27
+// Date   12/02/19
+
+
+	class programa8JAVA{
+	 
+		public static void main(String[] args){
+
+		System.out.println("     J      A   V     V   A");
+		System.out.println("     J     A A   V   V   A A");
+		System.out.println(" J   J    AAAAA   V V   AAAAA");
+		System.out.println("  JJ     A     A   V   A     A");
+
+}
+}
+
+
