952db01fb3ba36a0c21e3122e7237c725fa1c959
al150417-Fernando-Cisneros <47366643+al150417-Fernando-Cisneros@users.noreply.github.com>
2019-02-12 13:26:56 -0700

    Add files via upload

---
+++ Programacion unidad1/programa7for.java
@@ -0,0 +1,21 @@
+// Fernando Joel Cisneros 
+// Time   13:19
+// Date   12/02/19
+
+import java.util.Scanner;
+
+	class programa7for{
+	 
+		public static void main(String[] args){
+
+		   Scanner in = new Scanner(System.in);
+			System.out.println("Dame el primer numero a multiplicar");
+			int num1 = in.nextInt();
+		for (int i=0; (i<10); i++){
+			System.out.println(num1 + "*" + (i+1) + "=" + (num1 * (i+1)));
+}
+
+}
+}
+
+
