7987e7ae8ce0d0a943623a382930c641f15eec19
al150417-Fernando-Cisneros <47366643+al150417-Fernando-Cisneros@users.noreply.github.com>
2019-02-12 13:43:38 -0700

    Add files via upload

---
+++ Programacion unidad1/programa10formula.java
@@ -0,0 +1,15 @@
+// Fernando Joel Cisneros 
+// Time   13:40
+// Date   12/02/19
+
+
+	class programa10formula{
+	 
+		public static void main(String[] args){
+
+		System.out.println(4.0*(1-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11)));
+	
+}
+}
+
+
