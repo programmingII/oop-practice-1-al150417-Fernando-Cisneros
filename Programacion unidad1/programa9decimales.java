dfad1988f7914182e94f43bac190e868fd978bea
al150417-Fernando-Cisneros <47366643+al150417-Fernando-Cisneros@users.noreply.github.com>
2019-02-12 13:40:14 -0700

    Add files via upload

---
+++ Programacion unidad1/programa9decimales.java
@@ -0,0 +1,15 @@
+// Fernando Joel Cisneros 
+// Time   13:35
+// Date   12/02/19
+
+
+	class programa9decimales{
+	 
+		public static void main(String[] args){
+
+		System.out.println((25.5*3.5-3.5*3.5)/(40.5-4.5));
+	
+}
+}
+
+
