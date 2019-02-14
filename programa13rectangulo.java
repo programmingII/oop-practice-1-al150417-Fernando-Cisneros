b1397f8dcf020ea2f86e2cfae5abc17df2a812da
al150417-Fernando-Cisneros <47366643+al150417-Fernando-Cisneros@users.noreply.github.com>
2019-02-14 12:45:21 -0700

    Add files via upload

---
+++ programa13rectangulo.java
@@ -0,0 +1,21 @@
+// Fernando Joel Cisneros 
+// Time   12:35
+// Date   14/02/19
+
+
+
+	class programa13rectangulo{
+	 
+		public static void main(String[] args){     		//Empezamos el programa
+			
+			System.out.println("Ancho de un rectangulo: 5.6"); //imprime los valores de largo y ancho en las siguientes 2 lineas
+
+			System.out.println("Largo del rectangulo: 8.5");
+
+			System.out.println("El area del rectangulo es: 5.6*8.5=" + 5.6*8.5); //hace un calculo del area luego imprime el resultado
+
+			System.out.println("El perimetro del rectangulo es: 2*(5.6*8.5)=" + 2*(5.6*8.5)); //hace un calculo del perimetro y lo imprime al final
+
+			
+}
+}
