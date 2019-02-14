85cc1a5b6301d01ebd3b56f0f2dae62b6d9cb4ea
al150417-Fernando-Cisneros <47366643+al150417-Fernando-Cisneros@users.noreply.github.com>
2019-02-14 12:16:54 -0700

    Add files via upload

---
+++ Programacion unidad1/programa11.java
@@ -0,0 +1,15 @@
+// Fernando Joel Cisneros 
+// Time   12:10
+// Date   14/02/19
+
+
+	class programa11circulo{
+	 
+		public static void main(String[] args){
+
+		System.out.println("El radio es:" + "7.5");
+		System.out.println("Area:" + (3.1416*(7.5*7.5)));
+		System.out.println("Perimetro:" + (3.1416*7.5)); 
+	
+}
+}
