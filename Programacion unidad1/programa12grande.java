4746357254980b96ba1e79cdc6b8a2bc83a7c2bd
al150417-Fernando-Cisneros <47366643+al150417-Fernando-Cisneros@users.noreply.github.com>
2019-02-14 12:31:12 -0700

    Add files via upload

---
+++ Programacion unidad1/programa12grande.java
@@ -0,0 +1,29 @@
+// Fernando Joel Cisneros 
+// Time   12:19
+// Date   14/02/19
+
+import java.util.Scanner; //importamos una libreria para escanear numeros
+
+	class programa12grande{
+	 
+		public static void main(String[] args){     		//Empezamos el programa
+
+		   Scanner in = new Scanner(System.in);
+			System.out.println("Dame el primer numero");
+			int num1 = in.nextInt();
+			
+			System.out.println("Dame el segundo numero");
+			int num2 = in.nextInt();
+
+			System.out.println("Dame el tercer numero");
+			int num3 = in.nextInt();
+		
+			System.out.println("Dame el cuarto numero");
+			int num4 = in.nextInt();
+
+			System.out.println("Dame el quinto numero");
+			int num5 = in.nextInt();
+			
+			System.out.println("El promedio es:" + (num1+num2+num3+num4+num5)/5);
+}
+}
