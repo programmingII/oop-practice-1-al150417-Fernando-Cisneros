6df3184202010b9352e4c76cc4a4463b09d82ab8
al150417-Fernando-Cisneros <47366643+al150417-Fernando-Cisneros@users.noreply.github.com>
2019-02-14 12:52:21 -0700

    Add files via upload

---
+++ Programacion unidad1/programa14bandera.java
@@ -0,0 +1,27 @@
+// Fernando Joel Cisneros 
+// Time   12:45
+// Date   14/02/19
+
+
+
+	class programa14bandera{
+	 
+		public static void main(String[] args){     		//Empezamos el programa
+			  System.out.println("* * * * * * =================================="); // Estas lineas solo imprimen asteristos y linas para formar una bandera
+        		  System.out.println(" * * * * *  ==================================");
+       			  System.out.println("* * * * * * ==================================");
+        		  System.out.println(" * * * * *  ==================================");
+       			  System.out.println("* * * * * * ==================================");
+       			  System.out.println(" * * * * *  ==================================");
+       		 	  System.out.println("* * * * * * ==================================");
+       			  System.out.println(" * * * * *  ==================================");
+        		  System.out.println("* * * * * * ==================================");
+        		  System.out.println("==============================================");
+         		  System.out.println("==============================================");
+       			  System.out.println("==============================================");
+      			  System.out.println("==============================================");
+      			  System.out.println("==============================================");
+   		          System.out.println("==============================================");
+		
+}
+}
