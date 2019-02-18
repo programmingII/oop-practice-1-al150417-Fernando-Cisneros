75beedc9907a289c580970c3f8106cc1957cf624
al150417-Fernando-Cisneros <47366643+al150417-Fernando-Cisneros@users.noreply.github.com>
2019-02-18 10:13:54 -0700

    Add files via upload

---
+++ Programacion unidad1/programa17binario.java
@@ -0,0 +1,16 @@
+//Nombre:Fernando Cisneros 
+//Fecha: 18/02/2019
+//Hora: 10:02
+	import java.util.Scanner;
+		class programa17binarios{
+		public static void main(String[]args){
+		String x,y;
+		Scanner num = new Scanner(System.in);
+			System.out.println("Dame El primer numero binario");
+			x = num.nextLine();
+			System.out.println("Dame el segundo numero binario");
+			y = num.nextLine();
+			System.out.println("El resultado es: "+(Integer.toBinaryString(Integer.parseInt(x,2)+Integer.parseInt(y,2))));
+
+}
+}
